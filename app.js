9d4cb6a04bc6ef3706c1ec606ba88e7fddc01111
Lorenzo Miglietta <lorenzo.miglietta2500@gmail.com>
2022-07-14 11:26:42 +0200

    commit

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const express = require ("express");
-const path = require('path');
+const express = require("express");
+const axios = require('axios');
+
 const app = express();
 
 let port = process.env.PORT;
-if (port == null || port == ""){
+if (port == null || port == "") {
     port = 3000;
 }
 
-app.listen(port, ()=>{
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/index.html');
+});
+
+app.post('/', (req, res) => {
+    
+});
+
+app.listen(port, () => {
     console.log("Server started on port", port);
 })
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,5 +1,5 @@
 
-const ImageLoaderWorker = new Worker('/public/javascript/workers/image-loader.js');
+const ImageLoaderWorker = new Worker('/javascript/workers/image-loader.js');
 
 const imgElements = document.querySelectorAll('img[data-src]')
 
@@ -21,3 +21,38 @@ imgElements.forEach(imageElement => {
   const imageURL = imageElement.getAttribute('data-src')
   ImageLoaderWorker.postMessage(imageURL)
 })
+
+// ===========================================================
+let cards = $(document.querySelectorAll(".card"));
+let days = $(document.querySelectorAll(".giorno"));
+let weatherIcons = $(document.querySelectorAll(".icona-meteo"));
+let descriptions = $(document.querySelectorAll(".card-descrizione"));
+let maxTemps = $(document.querySelectorAll(".card-maxTemp"));
+let minTemps = $(document.querySelectorAll(".card-minTemp"));
+
+async function getTempo(city){
+  var str="http://api.openweathermap.org/geo/1.0/direct?q=Milano&appid=e06672eb3d022cf9dbdffb18d384e515";
+  var response = await
+  fetch(str,
+  {
+      method:"GET"
+  });
+  var jsonData = await response.json();
+  var lat = jsonData[0].lat.toPrecision(5);
+  var lon = jsonData[0].lon.toPrecision(5);
+  var url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=e06672eb3d022cf9dbdffb18d384e515";
+  var response = await
+  fetch (url,
+  {
+      method:"GET"
+  });
+  var jsonData = await response.json();
+  console.log(jsonData);
+  var temperaturaCelsius = jsonData.main.temp-273.15;
+  //document.getElementById("tempo").innerText = temperaturaCelsius.toPrecision(2)+"°";
+  console.log(temperaturaCelsius);
+}
+
+getTempo("Milano");
+
+console.log(days[0].innerText);
